d0f5828b5310c072d6a506a1f756d9c4b4973dd3
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-30 06:36:33 +0100

    Add likeRecipe, and unlikeRecipe

--- schema.js
+++ schema.js
@@ -40,15 +40,11 @@ type Token {
 
 type Mutation {
   addRecipe(name: String!, description: String!, category: String!, instructions: String!, username: String): Recipe
+  deleteUserRecipe(_id: ID): Recipe
+  likeRecipe(_id: ID!, username: String!): Recipe
+  unlikeRecipe(_id: ID!, username: String!): Recipe
   signinUser(username: String!, password: String!): Token
   signupUser(username: String!, email: String!, password: String!): Token
 }
 
 `;
-
-//   getCurrentUser: User
-
-// deleteUserRecipe(_id: ID): Recipe
-
-// likeRecipe(_id: ID!, username: String!): Recipe
-// unlikeRecipe(_id: ID!, username: String!): Recipe
